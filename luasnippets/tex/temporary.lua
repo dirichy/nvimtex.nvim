8f242afb5a7c1184e696bad699022dfa7f7c19cd
dirichy <byl315121270@gmail.com>
2025-10-24 09:59:19 +0800

    add a english snip

--- luasnippets/tex/temporary.lua
+++ luasnippets/tex/temporary.lua
@@ -3,18 +3,25 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 local fmta = require("luasnip.extras.fmt").fmta
+local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
 local tex = require("nvimtex.conditions.luasnip")
 
 return {
-	-- s(
-	-- 	{ trig = "test", snippetType = "autosnippet" },
-	-- 	fmta("\\<>{<>}", {
-	-- 		i(1),
-	-- 		f(function()
-	-- 			return vim.fn.input("input something")
-	-- 		end),
-	-- 	}),
-	-- 	{ condition = tex.in_math }
-	-- ),
+	s(
+		{ trig = "eng", snippetType = "autosnippet" },
+		fmta("\\item \\eng{<>} <>", {
+			i(1),
+			i(2),
+		}),
+		{ condition = line_begin * tex.in_text }
+	),
+	s(
+		{ trig = "  eng", snippetType = "autosnippet" },
+		fmta("\\item \\eng{<>} <>", {
+			i(1),
+			i(2),
+		}),
+		{ condition = line_begin * tex.in_text }
+	),
 }
