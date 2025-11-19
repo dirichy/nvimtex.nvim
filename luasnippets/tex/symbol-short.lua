77fc1f132da7f3ef1fd9f0335272b96215eb2790
evawyy <evawyy@12345>
2025-11-19 12:17:51 +0800

    add cdot snip

--- luasnippets/tex/symbol-short.lua
+++ luasnippets/tex/symbol-short.lua
@@ -1,40 +1,47 @@
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
+local f = ls.function_node
 
-local tex =require("nvimtex.conditions.luasnip")
+local tex = require("nvimtex.conditions.luasnip")
 return {
-  s({ trig = "...", wordTrig = false, snippetType = "autosnippet" }, {
-    t("\\cdots"),
-  }, { condition = tex.in_math }),
-  s({ trig = ";;;", wordTrig = false, snippetType = "autosnippet" }, {
-    t("\\vdots"),
-  }, { condition = tex.in_math }),
-  s({ trig = "==", wordTrig = false, snippetType = "autosnippet" }, {
-    t("&="),
-  }, { condition = tex.in_math }),
-  s({ trig = "~", wordTrig = false, snippetType = "autosnippet" }, {
-    t("\\sim"),
-  }, { condition = tex.in_math }),
-  s({ trig = "--", wordTrig = false, snippetType = "autosnippet" }, {
-    t("\\setminus"),
-  }, { condition = tex.in_math }),
-  s({ trig = "=>", wordTrig = false, snippetType = "autosnippet" }, {
-    t("\\implies"),
-  }, { condition = tex.in_math }),
-  s({ trig = "<=", snippetType = "autosnippet" }, {
-    t("\\impliedby"),
-  }, { condition = tex.in_math }),
-  s({ trig = "=>", wordTrig = false, snippetType = "autosnippet" }, {
-    t("``\\(\\implies\\)'':"),
-  }, { condition = tex.in_text }),
-  s({ trig = "<=", snippetType = "autosnippet" }, {
-    t("``\\(\\impliedby\\)'':"),
-  }, { condition = tex.in_text }),
-  s({ trig = "##", wordTrig = false, snippetType = "autosnippet" }, {
-    t("\\# "),
-  }, { condition = tex.in_math }),
-  s({ trig = "0set", snippetType = "autosnippet", priority = 2000 }, {
-    t("\\varnothing"),
-  }, { condition = tex.in_math }),
+	s({ trig = "%.%.([^%.])", wordTrig = false, snippetType = "autosnippet", priority = 1, regTrig = true }, {
+		t("\\cdot"),
+		f(function(_, snip)
+			return snip.captures[1]
+		end),
+	}, { condition = tex.in_math }),
+	s({ trig = "...", wordTrig = false, snippetType = "autosnippet" }, {
+		t("\\cdots"),
+	}, { condition = tex.in_math }),
+	s({ trig = ";;;", wordTrig = false, snippetType = "autosnippet" }, {
+		t("\\vdots"),
+	}, { condition = tex.in_math }),
+	s({ trig = "==", wordTrig = false, snippetType = "autosnippet" }, {
+		t("&="),
+	}, { condition = tex.in_math }),
+	s({ trig = "~", wordTrig = false, snippetType = "autosnippet" }, {
+		t("\\sim"),
+	}, { condition = tex.in_math }),
+	s({ trig = "--", wordTrig = false, snippetType = "autosnippet" }, {
+		t("\\setminus"),
+	}, { condition = tex.in_math }),
+	s({ trig = "=>", wordTrig = false, snippetType = "autosnippet" }, {
+		t("\\implies"),
+	}, { condition = tex.in_math }),
+	s({ trig = "<=", snippetType = "autosnippet" }, {
+		t("\\impliedby"),
+	}, { condition = tex.in_math }),
+	s({ trig = "=>", wordTrig = false, snippetType = "autosnippet" }, {
+		t("``\\(\\implies\\)'':"),
+	}, { condition = tex.in_text }),
+	s({ trig = "<=", snippetType = "autosnippet" }, {
+		t("``\\(\\impliedby\\)'':"),
+	}, { condition = tex.in_text }),
+	s({ trig = "##", wordTrig = false, snippetType = "autosnippet" }, {
+		t("\\# "),
+	}, { condition = tex.in_math }),
+	s({ trig = "0set", snippetType = "autosnippet", priority = 2000 }, {
+		t("\\varnothing"),
+	}, { condition = tex.in_math }),
 }
